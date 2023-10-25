d1a05b1c9f8810a1bb681f433d9f1f3182af9583
Ruslan-Far <rfarhetdinov@mail.ru>
2023-10-25 21:29:47 +0300

    done tasks 1

---
+++ scripts/tasks_1/1/pub_1_1.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+
+import numpy as np
+import rospy
+from sensor_msgs.msg import Image
+from typing import Final
+
+ROS_NODE_NAME: Final[str] = "publisher"
+ROS_PARAM_PUB_RATE: Final[int] = 30
+ROS_IMAGE_TOPIC: Final[str] = "image"
+
+def generate_image(height, width):
+	img_mono8 = np.random.randint(0, 256, (height * width))
+	return img_mono8.tolist()
+
+
+def main() -> None:
+  rospy.init_node(ROS_NODE_NAME)
+
+  pub_frequency: int = rospy.get_param("~rate", ROS_PARAM_PUB_RATE)
+
+  publisher = rospy.Publisher(ROS_IMAGE_TOPIC, Image, queue_size = 10)
+
+  rospy.loginfo(f"Publishing to '{rospy.resolve_name(ROS_IMAGE_TOPIC)}' at {pub_frequency} Hz ...")
+
+  height = 240
+  width = 320
+
+  rate = rospy.Rate(pub_frequency)
+
+  while not rospy.is_shutdown():
+
+    publisher.publish(Image(height = height, width = width, encoding = 'mono8', data = generate_image(height, width)))
+    
+    rate.sleep()
+
+
+if __name__ == '__main__':
+    main()
---
+++ scripts/tasks_1/3/sub_1_3.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+
+import cv2
+import rospy
+from cv_bridge import CvBridge
+from sensor_msgs.msg import Image
+from typing import Final
+
+ROS_NODE_NAME: Final[str] = "subscriber"
+ROS_IMAGE_TOPIC: Final[str] = "/pylon_camera_node/image_raw"
+
+def image_callback(msg: Image, cv_bridge: CvBridge) -> None:
+  image = cv_bridge.imgmsg_to_cv2(msg)
+  window_name = 'window'
+  cv2.imshow(window_name, image)
+  cv2.waitKey(1)
+
+
+def main() -> None:
+  rospy.init_node(ROS_NODE_NAME)
+
+  sample: Image = rospy.wait_for_message(ROS_IMAGE_TOPIC, Image, timeout = 3.0)
+  
+  if sample is not None:
+    rospy.loginfo(f"Encoding: {sample.encoding}, Resolution: {sample.width, sample.height}")
+  
+  cv_bridge: CvBridge = CvBridge()
+
+  rospy.Subscriber(ROS_IMAGE_TOPIC, Image, lambda msg: image_callback(msg, cv_bridge), queue_size = None)
+
+  rospy.spin()
+
+
+if __name__ == '__main__':
+  main()
