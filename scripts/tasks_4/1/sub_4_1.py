0657ffd3c8b6e512ab50dfe8834cfcc88608f638
Ruslan-Far <rfarhetdinov@mail.ru>
2023-11-29 23:01:34 +0300

    добавил комментарии в коде по результатам экспериментов

--- scripts/tasks_4/1/sub_4_1.py
+++ scripts/tasks_4/1/sub_4_1.py
@@ -11,15 +11,7 @@ ROS_IMAGE_TOPIC: Final[str] = "/pylon_camera_node/image_raw"
 HEIGHT: Final[int] = 300
 WIDTH: Final[int] = 300
 WINDOW_ORIG: Final[str] = "original"
-WINDOW_CANNY: Final[str] = "canny"
-WINDOW_SOBEL: Final[str] = "sobel"
-WINDOW_BIN_THRESH_CANNY_RGB: Final[str] = "bin_thresh_canny_rgb"
-# WINDOW_CLAHE: Final[str] = "clahe"
-WINDOW_FILTERED_GAUSSIAN: Final[str] = "filtered_gaussian"
-WINDOW_FILTERED_MEDIAN: Final[str] = "filtered_median"
-WINDOW_FILTERED_BOX: Final[str] = "filtered_box"
-WINDOW_IMPROVED: Final[str] = "improved"
-WINDOW_FILTERED_IMPROVED: Final[str] = "filtered_improved"
+WINDOW_IMPROVED_CANNY: Final[str] = "improved_canny"
 
 
 
@@ -32,7 +24,16 @@ def image_callback(msg: Image, cv_bridge: CvBridge) -> None:
 	img_gray = cv2.cvtColor(img_rgb, cv2.COLOR_RGB2GRAY)
 	img_hsv = cv2.cvtColor(img_rgb, cv2.COLOR_RGB2HSV)
 
-	img_canny = cv2.Canny(img_gray, 120, 300)
+	# Попробовал разные фильтры и везде результат был хуже
+
+	# kernel_size = (3, 3)
+	# img_gaussian_blur_filter = cv2.GaussianBlur(img_gray, kernel_size, 0)
+	# img_median_blur_filter = cv2.medianBlur(img_gray, kernel_size[0])
+	# img_box_filter = cv2.boxFilter(src = img_gray, ddepth = 0, ksize = kernel_size)
+
+	# Попробовал применить различные способы обнаружения контуров сначала без увеличения контрастности
+
+	# img_canny = cv2.Canny(img_gray, 120, 300)
 	# img_sobel = cv2.Sobel(img_gray, -1, dx=1, dy=1, ksize=5, scale=3)
 	# _, img_bin_thresh = cv2.threshold(img_gray, 50, 255, cv2.THRESH_BINARY)
 	# img_bin_thresh_canny = cv2.Canny(img_bin_thresh, 120, 300)
@@ -40,41 +41,29 @@ def image_callback(msg: Image, cv_bridge: CvBridge) -> None:
 	# img_bin_thresh_canny_rgb = img_rgb.copy()
 	# cv2.drawContours(img_bin_thresh_canny_rgb, contours, -1, (0,255,0), 1)
 
+	# Увеличил контрастность
+
 	clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(8,8))
 	img_adaptive_equalized = clahe.apply(img_gray)
 
-	img_improved = cv2.Canny(img_adaptive_equalized, 120, 300)
-	# img_improved = cv2.Sobel(img_adaptive_equalized, -1, dx=1, dy=1, ksize=5, scale=3)
+	# Попробовал применить различные способы обнаружения контуров теперь с увеличением контрастности, и результат стал намного лучше
+	# Дальнейшую работу буду проводить с Canny
+
+	img_improved_canny = cv2.Canny(img_adaptive_equalized, 120, 300)
+	# img_improved_sobel = cv2.Sobel(img_adaptive_equalized, -1, dx=1, dy=1, ksize=5, scale=3)
 	# _, img_bin_thresh = cv2.threshold(img_adaptive_equalized, 50, 255, cv2.THRESH_BINARY)
 	# img_bin_thresh_canny = cv2.Canny(img_bin_thresh, 120, 300)
 	# contours, _ = cv2.findContours(img_bin_thresh_canny, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
-	# img_improved = img_rgb.copy()
-	# cv2.drawContours(img_improved, contours, -1, (0,255,0), 1)
+	# img_improved_bin_thresh_canny = img_rgb.copy()
+	# cv2.drawContours(img_improved_bin_thresh_canny, contours, -1, (0,255,0), 1)
 
-	kernel_size = (3, 3)
-	# img_gaussian_blur_filter = cv2.GaussianBlur(img_gray, kernel_size, 0)
-	# img_median_blur_filter = cv2.medianBlur(img_gray, kernel_size[0])
-	img_box_filter = cv2.boxFilter(src = img_gray, ddepth = 0, ksize = kernel_size)
+	# Если улучшить контраст отфильтрованного изображения, то результат все равно будет хуже, чем без фильтрации
 
 	# img_adaptive_equalized = clahe.apply(img_box_filter)
-
-	# img_filtered_improved = cv2.Canny(img_adaptive_equalized, 120, 300)
-
-	img_filtered_improved = cv2.Canny(img_box_filter, 120, 300)
-
+	# img_filtered_improved_canny = cv2.Canny(img_adaptive_equalized, 120, 300)
 
 	cv2.imshow(WINDOW_ORIG, img_rgb)
-	cv2.imshow(WINDOW_CANNY, img_canny)
-	# cv2.imshow(WINDOW_SOBEL, img_sobel)
-	# cv2.imshow(WINDOW_BIN_THRESH_CANNY_RGB, img_bin_thresh_canny_rgb)
-	cv2.imshow(WINDOW_IMPROVED, img_improved)
-	cv2.imshow(WINDOW_FILTERED_IMPROVED, img_filtered_improved)
-
-	# cv2.imshow(WINDOW_ORIG, img_gray)
-	# cv2.imshow(WINDOW_CLAHE, img_adaptive_equalized)
-	# cv2.imshow(WINDOW_FILTERED_GAUSSIAN, img_gaussian_blur_filter)
-	# cv2.imshow(WINDOW_FILTERED_MEDIAN, img_median_blur_filter)
-	# cv2.imshow(WINDOW_FILTERED_BOX, img_box_filter)
+	cv2.imshow(WINDOW_IMPROVED_CANNY, img_improved_canny)
 
 	cv2.waitKey(1)
 
